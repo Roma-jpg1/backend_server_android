e0e834c8a1585d19f7896565573bd7d0365c8d9a
Roma-jpg1 <rpetrov2006@mail.ru>
2025-12-11 14:00:25 +0700

    link with sock

--- examples/server.py
+++ examples/server.py
@@ -1,24 +1,19 @@
-import socket
-    
-# Создаем сокет
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-# Привязываем сокет к IP-адресу и порту
-server_socket.bind(('localhost', 8080))
-    
-# Слушаем входящие соединения
-server_socket.listen(1)
-    
-print("Сервер запущен и ожидает подключений...")
-    
-# Принимаем входящее соединение
-client_socket, client_address = server_socket.accept()
-print(f"Подключение установлено с {client_address}")
-    
-# Получаем данные от клиента
-data = client_socket.recv(1024)
-print(f"Получены данные: {data}")
-    
-# Закрываем соединения
-client_socket.close()
-server_socket.close()
+import zmq
+import time
+PORT = 9560
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+
+socket.bind(f"tcp://*:{PORT}")
+
+print(f"на порте {PORT}")
+
+while True:
+    message = socket.recv()
+    print(f"[SERVER] Received from Android: {message.decode('utf-8')}")
+
+    time.sleep(1)
+
+    reply = "Hello from Python server!"
+    socket.send(reply.encode("utf-8"))
+
